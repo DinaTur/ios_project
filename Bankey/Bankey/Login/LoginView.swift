4fa2e0c7a1d88fb80159680fd2f44f7670fbeced
DinaTur <dina-turman@mail.ru>
2022-02-27 00:44:30 +0600

    Add onboarding view controller

--- Bankey/Bankey/Login/LoginView.swift
+++ Bankey/Bankey/Login/LoginView.swift
@@ -10,12 +10,10 @@ import UIKit
 
 class LoginView: UIView {
     
-    //let titleLabel = UILabel()
-    //let synopsisLabel = UILabel()
+
     let usernameTextField = UITextField()
     let passwordTextField = UITextField()
-    let stackViewLabel = UIStackView()
-    let stackViewText = UIStackView()
+    let stackView = UIStackView()
     let dividerView = UIView()
     
     override init(frame: CGRect) {
@@ -36,26 +34,9 @@ extension LoginView {
         translatesAutoresizingMaskIntoConstraints = false
         backgroundColor = .secondarySystemBackground
         
-        stackViewText.translatesAutoresizingMaskIntoConstraints = false
-        stackViewText.axis = .vertical
-        stackViewText.spacing = 8
-        
-        /*stackViewLabel.translatesAutoresizingMaskIntoConstraints = false
-        stackViewLabel.axis = .vertical
-        stackViewLabel.spacing = 10
-        
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        titleLabel.text = "Bankey"
-        titleLabel.textAlignment = .center
-        titleLabel.font = .systemFont(ofSize: 32, weight: .bold)
-        titleLabel.textColor = .black
-        
-        synopsisLabel.translatesAutoresizingMaskIntoConstraints = false
-        synopsisLabel.text = "Your premium source for all things banking"
-        synopsisLabel.textAlignment = .center
-        synopsisLabel.numberOfLines = 0
-        synopsisLabel.font = .systemFont(ofSize: 16, weight: .regular)
-        synopsisLabel.textColor = .black*/
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 8
         
         usernameTextField.translatesAutoresizingMaskIntoConstraints = false
         usernameTextField.placeholder = "Username"
@@ -76,26 +57,18 @@ extension LoginView {
     func layout() {
         //stackViewLabel.addArrangedSubview(titleLabel)
         //stackViewLabel.addArrangedSubview(synopsisLabel)
-        stackViewText.addArrangedSubview(usernameTextField)
-        stackViewText.addArrangedSubview(dividerView)
-        stackViewText.addArrangedSubview(passwordTextField)
+        stackView.addArrangedSubview(usernameTextField)
+        stackView.addArrangedSubview(dividerView)
+        stackView.addArrangedSubview(passwordTextField)
         //self.addSubview(stackViewLabel)
-        self.addSubview(stackViewText)
+        self.addSubview(stackView)
         
-        
-        /*NSLayoutConstraint.activate([
-            //stackViewLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
-            stackViewLabel.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 1),
-            trailingAnchor.constraint(equalToSystemSpacingAfter: stackViewLabel.trailingAnchor, multiplier: 1),
-            stackViewLabel.bottomAnchor.constraint(equalToSystemSpacingBelow: stackViewText.topAnchor, multiplier: 10)
-            
-        ])*/
         
         NSLayoutConstraint.activate([
-            stackViewText.topAnchor.constraint(equalToSystemSpacingBelow: topAnchor, multiplier: 1),
-            stackViewText.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 1),
-            trailingAnchor.constraint(equalToSystemSpacingAfter: stackViewText.trailingAnchor, multiplier: 1),
-            bottomAnchor.constraint(equalToSystemSpacingBelow: stackViewText.bottomAnchor, multiplier: 1)
+            stackView.topAnchor.constraint(equalToSystemSpacingBelow: topAnchor, multiplier: 1),
+            stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 1),
+            trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 1),
+            bottomAnchor.constraint(equalToSystemSpacingBelow: stackView.bottomAnchor, multiplier: 1)
         ])
         
         dividerView.heightAnchor.constraint(equalToConstant: 1).isActive = true
--- Bankey/Bankey/Login/LoginViewController.swift
+++ Bankey/Bankey/Login/LoginViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    let titleLabel = UILabel()
+    let subtitleLabel = UILabel()
     let loginView = LoginView()
     let signInButton = UIButton(type: .system)
     let errorMessagelabel = UILabel()
@@ -33,6 +35,19 @@ class LoginViewController: UIViewController {
 extension LoginViewController {
     
     private func style() {
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.textAlignment = .center
+        titleLabel.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        titleLabel.adjustsFontForContentSizeCategory = true
+        titleLabel.text = "Bankey"
+
+        subtitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        subtitleLabel.textAlignment = .center
+        subtitleLabel.font = UIFont.preferredFont(forTextStyle: .title3)
+        subtitleLabel.adjustsFontForContentSizeCategory = true
+        subtitleLabel.numberOfLines = 0
+        subtitleLabel.text = "Your premium source for all things banking!"
+        
         loginView.translatesAutoresizingMaskIntoConstraints = false
         
         signInButton.translatesAutoresizingMaskIntoConstraints = false
@@ -52,9 +67,23 @@ extension LoginViewController {
         view.addSubview(loginView)
         view.addSubview(signInButton)
         view.addSubview(errorMessagelabel)
+        view.addSubview(titleLabel)
+        view.addSubview(subtitleLabel)
         
+        //TitleLabel
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: loginView.leadingAnchor),
+            loginView.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor)
+        ])
+        //subtitleLabel
+        NSLayoutConstraint.activate([
+            subtitleLabel.topAnchor.constraint(equalToSystemSpacingBelow: titleLabel.bottomAnchor, multiplier: 3),
+            subtitleLabel.leadingAnchor.constraint(equalTo: loginView.leadingAnchor),
+            loginView.trailingAnchor.constraint(equalTo: subtitleLabel.trailingAnchor)
+        ])
         //LoginView
         NSLayoutConstraint.activate([
+            loginView.topAnchor.constraint(equalToSystemSpacingBelow: subtitleLabel.bottomAnchor, multiplier: 3),
             loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
             view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1)
