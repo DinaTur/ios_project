a19f3c4e8f5be95e31a519903c9149122800caa9
DinaTur <dina-turman@mail.ru>
2022-02-27 15:49:12 +0600

    refactor:create generic onboarding view controller
     This commit as a generic onboarding view controller that can be reused across multiple onboarding screens.

--- Bankey/Bankey/Onboarding/OnboardingViewController.swift
+++ Bankey/Bankey/Onboarding/OnboardingViewController.swift
@@ -14,30 +14,46 @@ class OnboardingViewController: UIViewController {
     let imageView = UIImageView()
     let textLabel = UILabel()
     
+    let heroImageName: String
+    let titleText: String
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         style()
         layout()
+    }
+    
+    init(heroImageName:String, titleText: String) {
+        self.heroImageName = heroImageName
+        self.titleText = titleText
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
 
 extension OnboardingViewController {
     
     func style(){
+        view.backgroundColor = .systemBackground
+        
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
         stackView.spacing = 20
         //image
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
-        imageView.image = UIImage(named: "delorean")
+        imageView.image = UIImage(named: heroImageName) //"delorean"
         //label
         textLabel.translatesAutoresizingMaskIntoConstraints = false
         textLabel.textAlignment = .center
         textLabel.font = UIFont.preferredFont(forTextStyle: .title3)
         textLabel.adjustsFontForContentSizeCategory = true
         textLabel.numberOfLines = 0
-        textLabel.text = "Bankey is faster, easier to use, and has a brand new look and feel that will make you feel like you are back in 1989."
+        textLabel.text = titleText//"Bankey is faster, easier to use, and has a brand new look and feel that will make you feel like you are back in 1989."
     }
     
     func layout() {
